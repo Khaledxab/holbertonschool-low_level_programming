0c216615c23cfd6e894db7e9f16eedbdfe5197b4
Khaledxab <khaledxab>
2021-10-01 14:08:35 -0700

    count

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Count to 10
+ * * Return: 0
+ */
+int main(void)
+{
+Int x;
+for (x == 0 ; x <= 10 ; x++)
+printf("%d\n", x);
+return (0);
+}
