7f2dec3c8da6438055ae3e5e658f7cd2eddc11cb
Khaledxab <khaledxab>
2021-10-01 11:37:21 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - positive negative
+* Return: 0
+*/
+int main(void)
+{
+for (int x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+}
