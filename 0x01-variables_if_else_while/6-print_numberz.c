f08bc7494242dac0ecc93065d70994efacec647d
Khaledxab <khaledxab>
2021-10-01 14:49:04 -0700

    Numberzzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Count to 10
+ * * Return: 0
+ */
+int main(void)
+{
+int x = '0';
+while (x <= '9')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
