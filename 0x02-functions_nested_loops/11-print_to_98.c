7b0e1a44678590149e6e0a05e00e8837620edcdb
Khaledxab <khaledxab>
2021-10-05 15:52:50 -0700

    print98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,28 +7,24 @@
  */
 void print_to_98(int n)
 {
-	if  (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	
-	else if (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	else
-	{
-		printf("98");
-	}
-	printf("\n");
+if  (n < 98)
+{
+while (n < 98)
+{
+printf("%d", n);
+printf(", ");
+n++;
 }
+}
+else
+{
+while (n > 98)
+{
+printf("%d", n);
+printf(", ");
+n--;
+}
+}
+printf("98\n");
+}
+
