6a016819e433708e41c1b0c9c3723a0a363acfc7
Khaledxab <khaledxab>
2021-10-05 11:30:20 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_sign  - main.h / print if + or - or = 0 
+* @n: n to check
+* Return: 0
+*/
+int print_sign(int n)
+{
+int a = '+' ;
+int b = '0' ;
+int c = '-' ;
+if (n > 0)
+  {
+  return (1);
+_putchar(a);
+
+  }
+ else if (n == 0)
+   {
+return (0);
+_putchar (b);
+   }
+else if (n < 0)
+  {
+    return (-1);
+_putchar(c);
+  }
+}
