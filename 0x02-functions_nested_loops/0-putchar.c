f76ca6e85a69ff963922e15d17a08da559813e4f
Khaledxab <khaledxab>
2021-10-05 04:15:25 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int main (void)
+{
+char t[] = "_putchar" ;
+ int i = 0;
+while ( t[i] != '\0' ) 
+{
+_putchar(t[i]);
+ i++;
+}
+ _putchar('\n');
+ return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+LOOP AND LOOP
