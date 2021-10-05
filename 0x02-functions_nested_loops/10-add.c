60c897d3e0c7c2d2659fd0c80526b5ec8a57fc9e
Khaledxab <khaledxab>
2021-10-05 15:28:44 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* add  - main.h / add + 
-* @first: first number @second: second number 
+* add  - main.h / add +
+* @first: first number
+* @second: second number
 * Return: 0
 */
-int add(int first,int second)
+int add(int first, int second)
 {
 return (first + second);
 }
