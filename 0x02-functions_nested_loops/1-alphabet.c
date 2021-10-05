0d825d57e75aa3e0e42d2f9002e494aaf2fda47a
Khaledxab <khaledxab>
2021-10-05 04:44:27 -0700

    alphabet with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - main.h / alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int e = '\n';
+int x = 'a';
+while (x <= 'z')
+{
+_putchar(x);
+x++;
+}
+_putchar (e);
+}
