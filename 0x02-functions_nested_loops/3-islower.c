43317fd16aa9faf5017c7e5ba6fe52a17525d919
Khaledxab <khaledxab>
2021-10-05 10:37:52 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* print_alphabet - main.h / alphabet
+* _islower  - main.h / returns zero if c is not a lowercase letter
+* @c: c is an integer
 * Return: 0
 */
-int _islower(int c);
+int _islower(int c)
 {
 if (c > 96 && c < 123)
 return (1);
