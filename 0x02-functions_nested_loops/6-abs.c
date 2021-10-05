055563adbb0c54c959f6158d3bdd31e4ca726055
Khaledxab <khaledxab>
2021-10-05 12:11:42 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+
+0;10;1c#include "main.h"
+/**
+* _abs  - main.h / returns the absolute value of an integer
+* @n: n to check
+* Return: 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1 ;
+return(n);
+}
+else
+{
+return(n);
+}
+}
+   
