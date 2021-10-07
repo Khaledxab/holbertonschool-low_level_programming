85db94f2c6058ba6355c4b3f38b5c40f2d354538
Khaledxab <khaledxab>
2021-10-07 04:03:59 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_most_numbers - main.h / print numbers !2 !4
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+int i;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+_putchar (i + '0');
+i++;
+}
+_putchar ('\n');
+}
