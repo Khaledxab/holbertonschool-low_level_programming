20fb1a23d6c4a1042a7495b86bd3de90757c8552
Khaledxab <khaledxab>
2021-10-07 03:27:56 -0700

    multip

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* mul - main.h / multip           
+* mul - main.h / multip
 * @a: num
 * @b: num
 * Return: 0
 */
 int mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
