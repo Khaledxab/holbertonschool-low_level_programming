9a21118d859a956aa99262a5f9511569c6f0388c
Khaledxab <khaledxab>
2021-10-11 09:09:18 -0700

    ReverseMe

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <string.h>
 /**
-* print_rev - main.h / print a string
-* @s: string
+* rev_string - main.h / print then reverse a string.
+* @s: string.
 */
 void rev_string(char *s)
 {
 	char temp;
 	int i = 0;
 	int j = 0;
-	
-	j = strlen (s) -1;
+
+	j = strlen(s) - 1;
 
 		while (i < j)
 		{
