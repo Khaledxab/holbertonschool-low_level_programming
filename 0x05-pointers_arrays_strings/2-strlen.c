13fe2873b6bd7a37abc52df8c96b5710cd80d461
Khaledxab <khaledxab>
2021-10-11 06:15:25 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen - main.h / return the length
+* @s: string
+*
+* Return: length of @s.
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	return (i);
+}
