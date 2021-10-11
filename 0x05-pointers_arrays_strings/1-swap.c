41acf4121107e9494ccb08235983094caeffd06a
Khaledxab <khaledxab>
2021-10-11 05:23:15 -0700

    SwapMe

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap_int - main.h /  swap a and b
+* @a: integer to swap
+* @b: integer to swap
+*/
+void swap_int(int *a, int *b)
+{
+*a = 98;
+*b = 42;
+*a = 42;
+*b = 98;
+}
