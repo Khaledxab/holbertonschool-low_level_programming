c3407b79d9ef83ae65290412e17baf343c650926
Khaledxab <khaledxab>
2021-10-11 04:50:42 -0700

    commit reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-* reset_to_98 - main.h /  takes a pointer to an int as parameter and updates the value it points to to 98
-* @*n: dereference of n 
+* reset_to_98 - main.h /  takes a pointer to an int as parameter
+* and updates the value it points to to 98.
+* @*n: dereference of n
 */
 void reset_to_98(int *n)
 {
