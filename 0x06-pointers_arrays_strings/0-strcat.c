acd11315e2b7caad2cc2e1f1a102a0da5404908c
Khaledxab <khaledxab>
2021-10-14 03:10:45 -0700

    MixME

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strcat - Concatenates two strings
+  * @dest: destination string
+  * @src: source string
+  *
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+
+	dest[len] = '\0';
+	return (dest);
+}
