69bef568302f341d8758225f4fc1f93ce1b53d75
Khaledxab <khaledxab@gmail.com>
2021-10-20 20:04:16 +0100

    Print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
+ * _print_rev_recursion - print in reverse
  *
  * @s: string to print.
  *
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 	_print_rev_recursion(s + 1); 
 	_putchar(*s);
