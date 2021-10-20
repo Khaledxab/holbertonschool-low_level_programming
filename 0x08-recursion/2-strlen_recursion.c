4c08ea2635824d0b36d81e2eda6dfea5e2986f0a
Khaledxab <khaledxab@gmail.com>
2021-10-20 20:14:44 +0100

    Length ME

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,12 +4,12 @@
  * _strlen_recursion - length of a string
  *
  * @s: string to print.
- *
+ * Return: length s.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
