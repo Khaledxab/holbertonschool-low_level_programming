41837ab58fad962da2b55b365c2e7e100c8d2325
Khaledxab <khaledxab@gmail.com>
2021-10-22 21:42:04 +0100

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - prints the programs name.
+* @argc: size of the argv array.
+* @argv: array of size argc.
+* Return: 0
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
