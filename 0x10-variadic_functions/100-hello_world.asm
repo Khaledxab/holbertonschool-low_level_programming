7825e03ccfb0ebe2596f517699be84ab7cb839ad
Khaledxab <khaledxab@gmail.com>
2021-11-04 22:35:58 +0100

    HELLOWORLD

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,19 +1,16 @@
-;	File: 100-hello_world.asm
-;	Desc: 64-bit assembly program that prints
-;	Hello, world followed by a new line.
+;File: 100-hello_world.asm
+;Desc: 64-bit assembly program that prints
+;Hello, world followed by a new line.
 section .text
-		global main
-
+global main
 main:
-	mov edx,len
-	mov ecx,msg
-	mov ebx,1
-	mov eax,4
-	int 0x80
-
-	mov eax,0
-	int 0x80
-
+mov edx,len
+mov ecx,msg
+mov ebx,1
+mov eax,4
+int 0x80
+mov eax,0
+int 0x80
 section .data
-	msg: db 'Hello, world', 0xa
-	len: equ $ - msg
+msg: db 'Hello, world', 0xa
+len: equ $ - msg
