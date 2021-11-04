0cb336bdd272f25843ae8389c5b01dd57d3da92d
Khaledxab <khaledxab@gmail.com>
2021-11-04 22:42:48 +0100

    hello

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,11 +1,11 @@
 section .data
-	message db "Hello, World",10
+	message db "Hello, World",6
 section .text
 global main
 main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 17
+	mov rdx, 12
 	syscall
 	
