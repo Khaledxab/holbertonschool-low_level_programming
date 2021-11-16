176f53bdc3ee0e365141fe5cd27e5c1b2900e2e1
Khaledxab <khaledxab@gmail.com>
2021-11-16 20:26:18 +0100

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
  * @h :a variable that points to a listint_t struct.
  * Return: the number of nodes.
  */
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
 	int number = 0;
 
