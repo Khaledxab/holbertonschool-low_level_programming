db1f888ea681cea9fe97d56eec23d36f8b28b290
Khaledxab <khaledxab@gmail.com>
2021-11-16 22:56:04 +0100

    HEADER

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 void free_list(list_t *head);
+size_t print_listint(const listint_t *h)
+
 
 #endif
