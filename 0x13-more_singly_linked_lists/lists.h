4555541c2686f374718b5d35538e426b4809aca4
Khaledxab <khaledxab@gmail.com>
2021-11-16 19:30:42 +0100

    HEADER

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <string.h>
 #include <stdio.h>
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 int _putchar(char c);
 
 #endif
