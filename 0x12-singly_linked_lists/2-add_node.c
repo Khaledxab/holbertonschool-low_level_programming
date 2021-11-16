43cf71e1ddc13262e1e3c1670d1188f21921d3b8
Khaledxab <khaledxab@gmail.com>
2021-11-16 22:51:43 +0100

    New Node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: variable pointing to variable pointing to a struct
+ * @n: value of new node
+ * Return: the address of the new element
+ */
+list_t *add_node(list_t **head, const char *str);
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
