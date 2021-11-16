d6e4ba2394a50380d8f35e8a48b9981f24819c35
Khaledxab <khaledxab@gmail.com>
2021-11-16 19:36:32 +0100

    HEADER

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,15 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/* LIST */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 int _putchar(char c);
