206057620ca429c8c9f3ce27cb4366c4aa8087ec
Khaledxab <khaledxab@gmail.com>
2022-01-06 21:30:54 +0100

    dlistint

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sums  data of a dlistint_t list.
+ * @head: head of the dlistint_t list.
+ *
+ * Return: sum of data.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
