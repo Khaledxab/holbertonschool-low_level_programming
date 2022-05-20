a56e5f07d58e5b6d94ed8b60d5b2049de5c8acc9
khaledxab <khaledxab@gmail.com>
2022-05-20 19:17:29 +0100

    FIX ✔

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,10 +17,10 @@ int linear_search(int *array, size_t size, int value)
                 printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
                 if (array[i] == value)
-                return (i)
+                return (i);
         }
         
-        return (i)
+        return (i);
 
 
 }
